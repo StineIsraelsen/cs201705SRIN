7cc8bf276ef066ff90d210c12058f4c51fb34500
Stine <stine.m.israelsen@gmail.com>
2017-05-16 12:57:47 +0200

    ,

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module06OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Terning t;
+            t = new Terning();
+            t.Test();
+            t.værdi = 1;
+            t.værdi2 = 1;
+            int v = t.værdi;
+
+            Terning t2;
+            t2 = new Terning();
+            t2.værdi = 3;
+
+            Terning t3;
+            t3 = new Terning();
+            t3.værdi = 5;
+
+            Console.WriteLine(t.værdi + " " + t2.værdi + " " + t3.værdi);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp3/Terning.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module06OOP
+{
+    class Terning
+    {
+
+        private static System.Random rnd = new Random(); // sørger for at de tilfældige tal er tilfældie.
+
+        
+        //data/variabler
+        public int værdi; // kan kun læses inden for klassen
+
+        public int værdi2;  // kan læses uden for klassen
+
+        public void Ryst() {
+            this.værdi = rnd.Next(1, 7);
+        }
+
+        public void Test()
+        {
+            this.værdi = 1;
+
+        }
+    }
+}
