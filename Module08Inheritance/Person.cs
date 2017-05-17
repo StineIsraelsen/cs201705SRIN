07d2639390e02664d3568ba6396a990c3dc0502c
Stine <stine.m.israelsen@gmail.com>
2017-05-17 12:43:35 +0200

    -

---
+++ Module08Inheritance/Person.cs
@@ -0,0 +1,20 @@
+﻿]using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module08Inheritance
+{
+    public class Person
+    {
+        public string Fornavn { get; set; }
+
+        public string Efternavn { get; set; }
+
+        public string FuldtNavn()
+        {
+            return $"{Fornavn} {Efternavn}";
+        }
+    }
+}
---
+++ Module08Inheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module08Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person() { Fornavn="Stine", Efternavn="Israelsen"};
+            
+            Console.WriteLine(p1.FuldtNavn());
+        }
+    }
+}
